398bf5c5d68f32bcd357e961db28d285ae55e6ee
Trung Pham <hai-trung.pham@capgemini.com>
2025-08-19 01:31:45 +0200

    style(layout): Add background color to main container

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
       >
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
           <div className="relative flex flex-col h-screen">
-            <main className="container mx-auto max-w-7xl flex-grow">
+            <main className="container mx-auto max-w-7xl flex-grow bg-[#292c3e]">
               {children}
             </main>
           </div>
