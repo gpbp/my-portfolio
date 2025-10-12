8fcf2b5c22e9cfa167c8cc354436c81800951242
haitr <haitrung.pham6573@gmail.com>
2025-10-12 23:24:18 +0200

    refactor: update MyProjects component to fetch projects from API and improve loading state handling

--- components/my-projects/MyProjects.tsx
+++ components/my-projects/MyProjects.tsx
@@ -1,7 +1,10 @@
+"use client";
+
 import TechStack from "@/ui-components/tech-stack/TechStack";
 import SlidingCard from "@/ui-components/sliding-card/SlidingCard";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Carousel from "@/ui-components/carousel/Carousel";
+import { Skeleton } from "@heroui/skeleton";
 
 type Project = {
     id: number;
@@ -12,86 +15,90 @@ type Project = {
     projectUrl?: string;
     githubUrl?: string;
     onProduction: boolean;
-    shrunkImageBackgroundPosition: string;
 };
 
-const projects: Project[] = [
-    {
-        id: 1,
-        name: "Portfolio Website",
-        description: "A personal portfolio website to showcase my projects and experiences.",
-        techStack: ["React", "TypeScript", "Tailwind CSS"],
-        imageUrl: "/img/meshImageFrame.png",
-        projectUrl: "localhost:3000",
-        githubUrl: "https://github.com/gpbp/my-portfolio",
-        onProduction: true,
-        shrunkImageBackgroundPosition: "bg-center"
-    },
-    {
-        id: 2,
-        name: "UI Library",
-        description: "A UI library for React applications",
-        techStack: ["Next.js", "Node.js", "PostgreSQL"],
-        imageUrl: "/img/uiLibrary.png",
-        projectUrl: "https://your-taskmanager.com",
-        githubUrl: "https://github.com/gpbp/my-ui-library",
-        onProduction: false,
-        shrunkImageBackgroundPosition: "bg-center"
-    },
-    {
-        id: 3,
-        name: "Food ideas",
-        description: "A platform to explore and share food recipes.",
-        techStack: ["React", "Redux", "Tailwind CSS"],
-        imageUrl: "/img/foodIdeas.jpg",
-        projectUrl: "https://www.recipetineats.com/",
-        githubUrl: "https://github.com/gpbp/food-ideas",
-        onProduction: false,
-        shrunkImageBackgroundPosition: "bg-center"
-    }
-];
+type ProjectDisplay = Project & {
+  shrunkImageBackgroundPosition: string;
+}
 
 export default function MyProjects(): JSX.Element {
-  const cards = (
-      projects.map((project) => {
-        const content = (
-          <div className="text-wrap">{project.description}</div>
-        );
+  const [projects, setProjects] = useState<ProjectDisplay[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [cards, setCards] = useState<JSX.Element[]>([]);
+  const [error, setError] = useState<string | null>(null);
 
-        const header = (
-          <div>
-            <div className="text-xl">{project.name}</div>
-          </div>
-        );
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch('/api/projects');
+        if (response.ok) {
+          const data = await response.json();
+          setProjects(data);
+        }
+      } catch (error) {
+        setError("Failed to fetch projects");
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
 
-        const projectLink = project.onProduction ?
-                (<div className="mt-4">
-                    <span>Link to the website: <a href={project.projectUrl} className="text-black hover:bg-gradient-to-b from-blue-500 to-indigo-500 hover:text-transparent bg-clip-text">{project.name}</a></span>
-                </div>) : (<></>)
+   useEffect(() => {
+    if (loading || projects.length === 0) {
+      setCards([<Skeleton key={1} className="flex-1/3 h-100 rounded-xl hover:cursor-pointer">
+      </Skeleton>, <Skeleton key={2} className="flex-1/3 h-100 rounded-xl hover:cursor-pointer">
+      </Skeleton>, <Skeleton key={3} className="flex-1/3 h-100 rounded-xl hover:cursor-pointer">
+      </Skeleton>]);
+    } else {
+      setCards(
+        projects
+          ?.slice()
+          .sort((a, b) => a.id - b.id)
+          .map((project) => {
+          const content = (
+            <div className="text-wrap">{project.description}</div>
+          );
 
-        const footer = (
-          <div className="text-wrap">
-            {projectLink}
-            <div className="mt-4">
-                <span>Link to the Github'repository: <a href={project.githubUrl} className="text-black hover:bg-gradient-to-b from-blue-500 to-indigo-500 hover:text-transparent bg-clip-text">{project.githubUrl}</a></span>
+          const header = (
+            <div>
+              <div className="text-xl">{project.name}</div>
             </div>
-            <TechStack techStack={project.techStack}/>
-          </div>
-        );
+          );
 
-        return (
-          <SlidingCard key={project.id} header={header} content={content} footer={footer} imageUrl={project.imageUrl} className="flex-1/3 h-100" shrunkImageBackgroundPosition={project.shrunkImageBackgroundPosition} />
-        );
-    }));
-    return (
-      <div className="flex flex-col gap-y-2">
-          <div className="font-roboto-mono inline-block items-center justify-center px-8 py-4 text-4xl font-bold w-2/3">
-    A blend of <p className="bg-gradient-to-l from-teal-400 via-cyan-400 to-blue-500 inline-block text-transparent bg-clip-text">clean code</p>, <p className="bg-gradient-to-l from-teal-400 via-cyan-400 to-blue-500 inline text-transparent bg-clip-text">creative design</p>, and <p className="bg-gradient-to-l from-teal-400 via-cyan-400 to-blue-500 inline text-transparent bg-clip-text">practical problem-solving</p>.
-          </div>
-          <div className="font-roboto-mono inline-block items-center justify-center px-8 py-4 text-lg text-gray-500 font-bold w-1/2">
-          Welcome to my projects! Here you can find a selection of my work, showcasing my skills and creativity.</div>
-          <Carousel cards={cards} />
-      </div>
-    );
+          const projectLink = project.onProduction ?
+                  (<div className="mt-4">
+                      <span>Link to the website: <a href={project.projectUrl} className="text-black hover:bg-gradient-to-b from-blue-500 to-indigo-500 hover:text-transparent bg-clip-text">{project.name}</a></span>
+                  </div>) : (<></>)
+
+          const footer = (
+            <div className="text-wrap">
+              {projectLink}
+              <div className="mt-4">
+                  <span>Link to the Github'repository: <a href={project.githubUrl} className="text-black hover:bg-gradient-to-b from-blue-500 to-indigo-500 hover:text-transparent bg-clip-text">{project.githubUrl}</a></span>
+              </div>
+              <TechStack techStack={project.techStack}/>
+            </div>
+          );
+
+          return (
+            <SlidingCard key={project.id} header={header} content={content} footer={footer} imageUrl={project.imageUrl} className="flex-1/3 h-100" shrunkImageBackgroundPosition={project.shrunkImageBackgroundPosition} />
+          );
+        })
+      );
+    }
+  }, [loading]);
+
+  return (
+    <div className="flex flex-col gap-y-2">
+        <div className="font-roboto-mono inline-block items-center justify-center px-8 py-4 text-4xl font-bold w-2/3">
+  A blend of <p className="bg-gradient-to-l from-teal-400 via-cyan-400 to-blue-500 inline-block text-transparent bg-clip-text">clean code</p>, <p className="bg-gradient-to-l from-teal-400 via-cyan-400 to-blue-500 inline text-transparent bg-clip-text">creative design</p>, and <p className="bg-gradient-to-l from-teal-400 via-cyan-400 to-blue-500 inline text-transparent bg-clip-text">practical problem-solving</p>.
+        </div>
+        <div className="font-roboto-mono inline-block items-center justify-center px-8 py-4 text-lg text-gray-500 font-bold w-1/2">
+        Welcome to my projects! Here you can find a selection of my work, showcasing my skills and creativity.</div>
+        <Carousel cards={cards} />
+    </div>
+  );
 }
 
