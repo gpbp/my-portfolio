5d49bb207bb929617b9d5ae95e0982d0b8e0f303
haitr <haitrung.pham6573@gmail.com>
2025-10-17 23:32:51 +0200

    feat: add ECS task definitions for frontend development and production; update next.config.js for API URL handling

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-const apiUrl = process.env.MY_PORTFOLIO_BACKEND_API_URL || 'http://localhost:8080';
+const apiUrl = process.env.MY_PORTFOLIO_BACKEND_API_URL;
 
 const nextConfig = {
   output: 'standalone',
@@ -9,7 +9,7 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: `${apiUrl}/:path*`,
+        destination: `${apiUrl}/api/:path*`,
       },
     ]
   },
