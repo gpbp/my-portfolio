e0cdb654097ca7d2d7a77977437b7068f4dc7a61
haitr <haitrung.pham6573@gmail.com>
2025-10-18 21:58:36 +0200

    chore: remove outdated frontend deployment workflow and replace with buildspec for AWS integration

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-const apiUrl = process.env.MY_PORTFOLIO_BACKEND_API_URL;
+const apiUrl = process.env.MY_PORTFOLIO_BACKEND_API_URL || "http://localhost:8080";
 
 const nextConfig = {
   output: 'standalone',
